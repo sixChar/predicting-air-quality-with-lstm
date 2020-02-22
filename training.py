32559345cae1082614e79bde8206a769116a9a67
Colm Evans <sixChar@users.noreply.github.com>
2020-02-21 21:05:42 -0600

    Update training.py

--- training.py
+++ training.py
@@ -1,4 +1,4 @@
-from LSTMModel import *
+from lstm_model import *
 
 # Just a big number since the weights are saved and the training can be interrupted without losing much progress.
 TRAIN_STEPS = 10000000
